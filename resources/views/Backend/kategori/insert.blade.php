259650af123d63329bf01d212a577b6a8b26e2bd
rafifahrezi75 <110459675+rafifahrezi75@users.noreply.github.com>
2022-12-04 14:50:57 +0700

    Files upload

---
+++ resources/views/Backend/kategori/insert.blade.php
@@ -0,0 +1,30 @@
+@extends('Backend.back')
+
+@section('admincontent')
+
+<div class="row">
+    <div class="col-6">
+
+        <form action="{{ url('admin/kategori') }}" method="post">
+            @csrf
+
+            <div class="mt-2">
+                <label class="form-label" for="">Kategori</label>
+                <input class="form-control" value="{{ old('kategori') }}" type="text" name="kategori" id="">
+                <span class="text-danger">
+                    @error('kategori')
+                        {{ $message }}
+                    @enderror
+                </span>
+            </div>
+            
+            <div class="mt-4">
+                <button class="btn btn-primary" type="submit">Simpan</button>
+            </div>
+
+        </form>
+
+    </div>
+</div>
+
+@endsection
